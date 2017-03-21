92e6baee708dd40c4da7091e1aba28258b8cbef0
Mattias Arrelid <mattias@arrelid.com>
2017-03-21 10:53:05 +0100

    Setup screen saver to require password.

--- defaults.sh
+++ defaults.sh
@@ -37,6 +37,11 @@ defaults write com.apple.systemuiserver "NSStatusItem Visible com.apple.menuextr
 # Show Bluetooth in the menu bar
 defaults write com.apple.systemuiserver "NSStatusItem Visible com.apple.menuextra.bluetooth" -int 0
 
+# Setup the screen saver to require password after a few seconds
+defaults write com.apple.screensaver "askForPassword" -int 1
+defaults write com.apple.screensaver "askForPasswordDelay" -int 5
+defaults write com.apple.screensaver "tokenRemovalAction" -int 0
+
 # Use a dark menu bar / dock
 defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
 
