afe58d9d69b227dd3405e0c5ae822ccc7fd5b754
Mattias Arrelid <mattias@arrelid.com>
2020-06-21 20:02:56 +0200

    Source rbenv/pyenv the fish way.

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -3,3 +3,6 @@ set -x LC_ALL "en_US.UTF-8"
 set -x FZF_DEFAULT_OPTS "--layout=reverse --inline-info --cycle"
 set -gx EDITOR vim
 set -gx VISUAL vim
+
+source (pyenv init – | psub)
+source (rbenv init - | psub)
